8a1186ddf080c75e3e9724690e337f5444208cb9
thunor12 <lucasspiteri@protonmail.com>
2021-09-25 18:47:40 +0200

    update

--- graph_matrix.h
+++ graph_matrix.h
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
-#define NODE_INIT_SIZE 10
+#define NODE_INIT_SIZE 2
 
-#define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x])))))
+//#define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x])))))
+//#define COUNT_OF(x) sizeof(x) / sizeof(x[0])
 
 typedef struct MatrixGraph {
 	bool** matrix;
@@ -13,64 +15,121 @@ typedef struct MatrixGraph {
 	size_t node_capacity;
 } matrix_graph;
 
-void m_graph_init(matrix_graph m_graph) {
+void m_graph_init(matrix_graph* m_graph) {
 
-	m_graph.matrix = (bool **) malloc(NODE_INIT_SIZE * sizeof(bool *));
+	m_graph->matrix = (bool **) malloc(NODE_INIT_SIZE * sizeof(bool *));
 
-	if(m_graph.matrix == NULL) {
+	if(m_graph->matrix == NULL) {
 		printf("Could not allocate to matrix\n");
 		return;
 	}
 
 	for (size_t i = 0; i < NODE_INIT_SIZE; ++i) {
-		m_graph.matrix[i] = (bool *) calloc(NODE_INIT_SIZE, sizeof(bool));
-		if(m_graph.matrix[i] == NULL) {
+		m_graph->matrix[i] = (bool *) calloc(NODE_INIT_SIZE, sizeof(bool));
+		if(m_graph->matrix[i] == NULL) {
 			printf("Could not allocate to matrix row\n");
 			return;
 		}
 	}
 	
-	m_graph.node_ids = (char **) malloc(NODE_INIT_SIZE * sizeof(char *));
+	m_graph->node_ids = (char **) malloc(NODE_INIT_SIZE * sizeof(char *));
 
-	if(m_graph.node_ids == NULL) {
+	if(m_graph->node_ids == NULL) {
 		printf("Could not allocate to node id array\n");
 		return;
 	}
 
-	m_graph.node_count = 0;
-	m_graph.node_capacity = NODE_INIT_SIZE;
+	m_graph->node_count = 0;
+	m_graph->node_capacity = NODE_INIT_SIZE;
 
 }
 
-void m_graph_check_capacity(matrix_graph m_graph) {
-	if(m_graph.node_count < m_graph.node_capacity)
+void m_graph_check_capacity(matrix_graph* m_graph) {
+	if(m_graph->node_count < m_graph->node_capacity)
 		return;
 
-	for (size_t i = 0; i < m_graph.node_capacity; ++i) {
-		m_graph.matrix[i] = (bool *) realloc(m_graph.matrix[i], sizeof(bool) * m_graph.node_capacity * 2);			
-		if(m_graph.matrix[i] == NULL) {
+	for (size_t i = 0; i < m_graph->node_capacity; ++i) {
+		m_graph->matrix[i] = (bool *) realloc(m_graph->matrix[i], sizeof(bool) * m_graph->node_capacity * 2);			
+		if(m_graph->matrix[i] == NULL) {
 			printf("Failled to realloc matrix row\n");
 			return;
 		}
 	}
 
-	m_graph.matrix = (bool **) realloc(m_graph.matrix, sizeof(bool*) * m_graph.node_capacity * 2);
+	m_graph->matrix = (bool **) realloc(m_graph->matrix, sizeof(bool*) * m_graph->node_capacity * 2);
 		
-	if(m_graph.matrix == NULL) {
+	if(m_graph->matrix == NULL) {
 		printf("Failled to realloc matrix\n");
 		return;
 	}
 
-	m_graph.node_capacity *= 2;
+	m_graph->node_capacity *= 2;
+
+	//printf("Realloced\n");
 
 }
 
-void m_graph_add_node(char* node_id, matrix_graph m_graph) {
-	m_graph_check_capacity(m_graph);
+bool m_graph_node_is_in_graph(char* node_id, matrix_graph* m_graph) {
+	if(m_graph->node_count == 0) {
+		printf("No nodes in graph\n");
+		return false;
+	}
 
-	/*Check if node in graph*/
-	m_graph.node_ids[m_graph.node_count + 1] = node_id;
+	for (size_t i = 0; i < m_graph->node_count; ++i) {
+		//printf("Is %s = to %s\n", node_id, m_graph->node_ids[i]);
+		if(strcmp(node_id, m_graph->node_ids[i]) == 0){
+			return true;
+		}
+	}
+
+	return false;
+}
+
+size_t m_graph_add_node(char* node_id, matrix_graph* m_graph) {
+	int64_t raw_index = m_graph_get_node_index(node_id, m_graph);
+
+	if(raw_index >= 0) {
+		printf("Node: %s allready in graph\n", node_id);
+		return (size_t) raw_index;
+	}
+
+	m_graph_check_capacity(m_graph);
+	m_graph->node_ids[m_graph->node_count] = (char*) malloc(sizeof(node_id));
+	strcpy(m_graph->node_ids[m_graph->node_count], node_id);
+
+	printf("Added: %s\n\n", m_graph->node_ids[m_graph->node_count]);
+	m_graph->node_count++;
+	return m_graph->node_count - 1;
+}
+
+int64_t m_graph_get_node_index(char* node_id, matrix_graph* m_graph) {
+
+	if(m_graph->node_count == 0) {
+		printf("No nodes in graph\n");
+		return -1;
+
+	int64_t i;
+	bool found = false;
+
+	for (i = 0; i < m_graph->node_count; i++)
+	{
+		if(node_id == m_graph->node_ids[i]) {
+			found = true;
+			break;
+		}
+	}
+
+	if(!found) {
+		return -1;
+	}
+
+	return i;
+	
+}
+
+void m_graph_add_link(char* child_id, char* parent_id, matrix_graph* m_graph) {
+	m_graph_add_node(child_id, m_graph);
+	m_graph_add_node(parent_id, m_graph);
 }
 
 void m_graph_free(matrix_graph m_graph);
-
--- main.c
+++ main.c
@@ -3,14 +3,21 @@
 
 #include "graph_matrix.h"
 
-#define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x])))))
+//#define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x])))))
+#define COUNT_OF(x) ((sizeof(x)/sizeof(x[0])))
 
 int main(int argc, char** argv)
 {
 	matrix_graph m_graph;
-	m_graph_init(m_graph);
+	m_graph_init(&m_graph);
 
-	printf("%ld\n", COUNT_OF(m_graph.matrix));
+	size_t n1_ind = m_graph_add_node("n1", &m_graph);
+	size_t n2_ind = m_graph_add_node("n2", &m_graph);
+	size_t n3_ind = m_graph_add_node("n3", &m_graph);
+	size_t n4_ind = m_graph_add_node("n4", &m_graph);
+	size_t n5_ind = m_graph_add_node("n4", &m_graph);
+
+	printf("Node count: %ld\n", m_graph.node_count);
 
 	return EXIT_SUCCESS;
 }
